a707e4b0cf0dbaacd9a77806d67011363081ce66
Eric Bastos <ericbastoscm@gmail.com>
2021-06-18 04:41:03 -0300

    Fixed smartypants

--- education4all/blog/templatetags/markdown_extras.py
+++ education4all/blog/templatetags/markdown_extras.py
@@ -9,4 +9,4 @@ register = template.Library()
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.markdown(value, extensions=['extra'])
+    return md.markdown(value, extensions=['extra', 'smarty'])
