a8e1f4869f34207e01d9736dbbe215ecd5e25e7c
Henrique <hentt30@gmail.com>
2021-06-28 19:19:50 -0300

    [fix]: Adding host to allowed hosts

--- education4all/settings.py
+++ education4all/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://education4all1.herokuapp.com/']
+ALLOWED_HOSTS = ['https://education4all1.herokuapp.com']
 
 # Application definition
 
